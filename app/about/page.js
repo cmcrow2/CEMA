696073c18c42d864acec7f0d2775b32010f31645
Cam Crow <19ccrow99@gmail.com>
2024-09-20 08:21:45 -0500

    feat: added three remaining pages

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+export default function WhoWeAre() {
+  return (
+    <div>
+      <p>Hello from Who We Are</p>
+    </div>
+  );
+}
---
+++ app/sample/page.js
@@ -0,0 +1,7 @@
+export default function SampleQuery() {
+  return (
+    <div>
+      <p>Hello from Sample Query</p>
+    </div>
+  );
+}
