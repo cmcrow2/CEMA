a261e862985b439c36681e3a6139c6529699fe9e
Cam Crow <19ccrow99@gmail.com>
2024-10-14 20:01:18 -0500

    feat: added profile cards, styling, and optimized for phone screens

--- app/about/page.js
+++ app/about/page.js
@@ -1,96 +1,215 @@
+"use client";
+
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useState, useEffect } from "react";
+import { twMerge } from "tailwind-merge";
+
 import Image from "next/image";
-import gears from "@/images/gears.svg";
-import team from "@/images/team.svg";
-import profile from "@/images/profile.svg";
+import civileng from "@/images/civileng.jpeg";
+import ProfileCarousel from "../components/profile/ProfileCarousel";
 
 export default function WhoWeAre() {
+  // gsap.registerPlugin(ScrollTrigger);
+  const [height, setHeight] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const element = document.getElementById("header");
+      setHeight(element.offsetHeight.toString());
+    }
+  }, [height]);
+
+  const heightClass = `h-[calc(100vh-${height}px)]`;
+  const xlHeightClass = `xl:h-[calc(100vh-${height}px)]`;
+
   return (
-    <div className="flex flex-col text-xl">
-      <h1 className="text-3xl font-courier px-6 pt-10">Who We Are</h1>
-      <hr className="m-4 border-black"></hr>
-      <div className="px-6 pb-6">
-        <p>
-          We are a small group of civil and software engineers, who were annoyed
-          at having to flip through hundreds of pages of information overload to
-          find the exact graph or equation we needed for a project.
-        </p>
-        <p className="pt-4">
-          With AI advancing quicker than anyone would have thought, our
-          engineers at C.E.M.A. (&quot;see-muh&quot;) came up with an idea to
-          use this new technology to steeply shorten the workload for every
-          civil engineer.
-        </p>
+    <div
+      className={twMerge(
+        "snap-y snap-mandatory overflow-y-scroll text-base xl:text-lg",
+        heightClass
+      )}
+    >
+      <div className="flex flex-col xl:flex-row-reverse">
+        <div className="xl:w-2/5">
+          <Image
+            src={civileng}
+            alt="Civil Engineering"
+            className={twMerge(
+              "h-56 md:h-72 w-full sticky top-0 object-cover object-[right,0,bottom,35%]",
+              xlHeightClass
+            )}
+            priority
+          ></Image>
+        </div>
+        <div className="xl:w-3/5">
+          {/* Who We Are Section */}
+          <div
+            className={`pt-6 px-4 pb-20 md:w-[75%] md:m-auto md:pt-32 md:pb-10 lg:pt-40 lg:overflow-scroll lg:snap-always lg:snap-center ${xlHeightClass} xl:w-[65%] xl:m-auto`}
+          >
+            <h1 className="text-3xl font-courier pb-2 lg:text-4xl xl:text-5xl xl:pb-4">
+              Who We Are
+            </h1>
+            <hr className="border-black"></hr>
+            <div className="flex flex-col pt-4 xl:pt-8 gap-y-4">
+              <p>
+                We are a small group of civil and software engineers, who were
+                annoyed at having to flip through hundreds of pages of
+                information overload to find the exact graph or equation we
+                needed for a project.
+              </p>
+              <p>
+                With AI advancing quicker than anyone would have thought, our
+                engineers at Askitect came up with an idea to use this new
+                technology to shorten the workload for every civil engineer.
+              </p>
+            </div>
+            <div className="pt-6 flex flex-col gap-y-4 text-base md:flex-row md:gap-x-4 xl:pt-16">
+              <button className="px-4 py-2 border-[0.5px] border-black hover:bg-blue-100 hover:transition-all rounded-md flex flex-row gap-x-2 items-center">
+                Contact Us
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                    />
+                  </svg>
+                </span>
+              </button>
+              <a
+                href="/feedback"
+                className="px-4 py-2 border-[0.5px] border-black hover:bg-blue-100 hover:transition-all rounded-md flex flex-row gap-x-2 items-center"
+              >
+                Send Us Feedback
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          {/* What We Do Section */}
+          <div
+            className={`pt-6 px-4 pb-20 md:w-[75%] md:m-auto md:pt-32 md:pb-10 lg:pt-40 lg:overflow-scroll lg:snap-always lg:snap-center ${xlHeightClass} xl:w-[65%] xl:m-auto`}
+          >
+            <h2 className="text-3xl font-courier pb-2 lg:text-4xl xl:text-5xl xl:pb-4">
+              What We Do
+            </h2>
+            <hr className="border-black"></hr>
+            <div className="flex flex-col pt-4 gap-y-4">
+              <p className="pt-4">
+                We are currently designing and building an AI to be a Civil
+                Engineer&apos;s personal assistant.
+              </p>
+              <p>
+                Once provided with any formal manual (like the American
+                Institute of Steel Construction for example), our AI will be
+                able to read, understand, and answer user questions about the
+                document.
+              </p>
+              <p>
+                Simply upload your manual, then ask your assistant any questions
+                you have!
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-row items-center gap-x-6 px-6 pb-14">
-        <button className="bg-rose p-2 rounded-md text-mint">Contact Us</button>
-        <a href="/feedback" className="bg-rose p-2 rounded-md text-mint">
-          Send Us Feedback
-        </a>
+      {/* Our Team Section */}
+      <div
+        className={`px-4 pb-20 md:w-[75%] md:m-auto md:pt-32 md:pb-10 lg:overflow-scroll lg:snap-always lg:snap-center ${xlHeightClass}`}
+      >
+        <h2 className="text-3xl font-courier pb-2">Our Team</h2>
+        <hr className="border-black"></hr>
+        <ProfileCarousel></ProfileCarousel>
       </div>
-      <div className="flex justify-center items-center">
-        <Image
-          src={gears}
-          alt="Who we are"
-          className="responsive w-[50%] h-[50%]"
-          priority
-        ></Image>
+      {/* Contact Us Section */}
+      <div
+        className={`pb-28 px-4 md:w-[75%] md:m-auto md:pt-32 md:pb-32 lg:snap-always lg:overflow-scroll lg:snap-center  lg:pt-40 xl:w-[50%] ${xlHeightClass} xl:m-auto`}
+      >
+        <h2 className="text-3xl font-courier pb-2 w-72 lg:w-[30rem] xl:text-5xl xl:pb-4">
+          We&apos;d love to hear from you!
+        </h2>
+        <hr className="border-black"></hr>
+        <div className="flex flex-col pt-4 gap-y-4">
+          <p className="">
+            If you like what you see, or have any constructive feedback for us,
+            please take some time to fill out our{" "}
+            <a href="/feedback" className="text-blue">
+              form
+            </a>
+            !
+          </p>
+          <p className="">
+            Since we are currently in the early stages of development, we are
+            only working on this project part time. With your feedback, we would
+            be able to move to working on this full time!
+          </p>
+        </div>
+        <div className="pt-6 flex flex-col gap-y-4 text-base md:flex-row md:gap-x-4 xl:pt-16">
+          <button className="px-4 py-2 border-[0.5px] border-black hover:bg-blue-100 hover:transition-all rounded-md flex flex-row gap-x-2 items-center">
+            Contact Us
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                />
+              </svg>
+            </span>
+          </button>
+          <a
+            href="/feedback"
+            className="px-4 py-2 border-[0.5px] border-black hover:bg-blue-100 hover:transition-all rounded-md flex flex-row gap-x-2 items-center"
+          >
+            Send Us Feedback
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                />
+              </svg>
+            </span>
+          </a>
+        </div>
       </div>
-      <h2 className="text-3xl font-courier px-6 pt-4">What We Do</h2>
-      <hr className="m-4 border-black"></hr>
-      <p className="px-6 pb-10">
-        We are currently designing and building an AI to be a Civil
-        Engineer&apos;s personal assistant. Once provided with any formal manual
-        (like the American Institute of Steel Construction for example), our AI
-        will be able to read, understand, and answer user questions about the
-        document. Simply upload your manual, then ask your assistant any
-        questions you have!
-      </p>
-      <div className="flex justify-center items-center">
-        <Image
-          src={team}
-          alt="Our team"
-          className="responsive w-[75%] h-[75%] -my-10"
-          priority
-        ></Image>
-      </div>
-      <h2 className="text-3xl font-courier px-6">Our Team</h2>
-      <hr className="m-4 border-black"></hr>
-      <div className="px-6 flex flex-col justify-center items-center pt-8">
-        <Image
-          src={profile}
-          alt="sample profile picture"
-          className="w-[25%] h-[25%] rounded-full object-none"
-        ></Image>
-        <p className="pt-4">Cameron Crow</p>
-        <p className="pb-8 px-10">[PLACEHOLDER] Quick facts about Cam</p>
-        <Image
-          src={profile}
-          alt="sample profile picture"
-          className="w-[25%] h-[25%] rounded-full object-none"
-        ></Image>
-        <p className="pt-4">Benjamin McKeig</p>
-        <p className="px-10">[PLACEHOLDER] Quick facts about Ben</p>
-      </div>
-      <h2 className="text-3xl font-courier px-6 pt-20">
-        We&apos;d love to hear from you!
-      </h2>
-      <hr className="m-4 border-black"></hr>
-      <p className="px-6">
-        If you like what you see, or have any constructive feedback for us,
-        please take some time to fill out our{" "}
-        <a
-          href="/feedback"
-          className="text-blue underline decoration-blue underline-offset-2"
-        >
-          form
-        </a>
-        !
-      </p>
-      <p className="px-6 pt-4 pb-10">
-        Since we are currently in the early stages of development, we are only
-        working on this project part time. With your feedback, we would be able
-        to move to working on this full time!
-      </p>
     </div>
   );
 }
---
+++ app/components/profile/ProfileCarousel.js
@@ -0,0 +1,94 @@
+import teamMembers from "@/utils/team";
+import ProfileCard from "@/app/components/profile/ProfileCard";
+import { Carousel, IconButton } from "@material-tailwind/react";
+
+export default function ProfileCarousel() {
+  const profileCards = teamMembers.map((member) => {
+    return (
+      <ProfileCard
+        key={member.name}
+        name={member.name}
+        image={member.profilePic}
+        role={member.role}
+        linkedin={member.linkedin}
+        github={member.github}
+      ></ProfileCard>
+    );
+  });
+
+  return (
+    <>
+      <Carousel
+        prevArrow={({ handlePrev }) => (
+          <IconButton
+            variant="text"
+            color="black"
+            size="lg"
+            onClick={handlePrev}
+            className="!absolute top-2/4 left-4 -translate-y-2/4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+              />
+            </svg>
+          </IconButton>
+        )}
+        nextArrow={({ handleNext }) => (
+          <IconButton
+            variant="text"
+            color="black"
+            size="lg"
+            onClick={handleNext}
+            className="!absolute top-2/4 !right-4 -translate-y-2/4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </IconButton>
+        )}
+        navigation={({ setActiveIndex, activeIndex, length }) => (
+          <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+            {new Array(length).fill("").map((_, i) => (
+              <span
+                key={i}
+                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                  activeIndex === i ? "w-8 bg-black" : "w-4 bg-black/50"
+                }`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
+        )}
+        className="py-10 md:hidden"
+        autoplay={true}
+        loop={true}
+      >
+        {profileCards}
+      </Carousel>
+      <div className="hidden md:flex flex-row pt-10 gap-x-4 overflow-x-scroll">
+        {profileCards}
+      </div>
+    </>
+  );
+}
